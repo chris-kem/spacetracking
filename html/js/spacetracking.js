7adacee67de7ee67661bb6dca40f736d175d7319
chris-kem <c.kem@gmx.de>
2020-05-18 16:21:10 +0000

    upload 18.05.2020 18:21 kein guter Tag

--- html/js/spacetracking.js
+++ html/js/spacetracking.js
@@ -68,7 +68,8 @@ client.on('message', function(topic, msg) {
   //
   pos.x = parseFloat(pos.x) * 100;
   pos.y = can.height - parseFloat(pos.y) * 100;
-  //pos.y = parseFloat(pos.y) * 1000;
+  //pos.y = parseFloat(pos.y) * 100;
+  console.log(pos.x + ", " + pos.y);
 
   //
   // check if we received a valid position
@@ -129,10 +130,13 @@ client.on('message', function(topic, msg) {
     point.x = pos.x; point.y = pos.y;
     //var ctx = tracked[el.id].getContext("2d");
     var c = ctx.getImageData(point.x,point.y,1,1).data;
+    console.log(el.style);
     //tracked[el.id] = (c[3]/255.) - (0.21*c[0] + 0.72*c[1] + 0.07*c[2])/255;
     //tracked[el.id] = (0.21*c[0] + 0.72*c[1] + 0.07*c[2])/255;
-    //tracked[el.id] = (c[3]/255.);
-    tracked[el.id] = 1 - 0.21*c[0] + 0.72*c[1] + 0.07*c[2];
+    tracked[el.id] = (c[3]/255.);
+    //tracked[el.id] = 1 - 0.21*c[0] + 0.72*c[1] + 0.07*c[2];
+    console.log(c[0] + ", " + c[0] + ", " + c[1] + ", " + c[2] + ", " + c[3]);
+    console.log(tracked[el.id]);
   }
 
   var o = new Object();
