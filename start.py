faefdc9b5ae5d1f2a1bbc278431650b1b940c5af
chris-kem <c.kem@gmx.de>
2020-06-29 10:58:54 +0000

    something changed

--- start.py
+++ start.py
@@ -1,18 +1,18 @@
 import subprocess
 import time
 
-p1 = subprocess.Popen('sensor/seriell-mqtt.py')
+p1 = subprocess.Popen(['sensor/seriell-mqtt.py'])
 time.sleep(1)
-p2 = subprocess.Popen('mqtt-linear.py')
+p2 = subprocess.Popen(['mqtt-linear.py'])
 time.sleep(1)
-p3 = subprocess.Popen('mqtt-binary.py')
+p3 = subprocess.Popen(['mqtt-binary.py'])
 time.sleep(1)
 
 try:
     while true:
-        print(running)
+        print('running')
         time.sleep(10)
-except: KeyboardInterrupt:
+except KeyboardInterrupt:
     p3.terminate()
     p2.terminate()
     p1.terminate()
