3b8ae20e9a5f3a15f32d6b2991c6bfeaa290feae
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-06-29 09:38:13 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,17 @@
-from subprocess import *
+import subprocess
+from pynput.keyboard import Key
 
-#Popen('python sensor/seriell-mqtt.py')
-subprocess.run('python mqtt-linear.py')
-subprocess.run('python mqtt-binary.py')
+p1 = subprocess.Popen('python sensor/seriell-mqtt.py')
+p2 = subprocess.Popen('python mqtt-linear.py')
+p3 = subprocess.Popen('python mqtt-binary.py')
+
+p1.terminate()
+p2.terminate()
+p3.terminate()
+
+def on_press(key):
+    print('{0} pressed'.format(key))
+    if key == '\x1b'
+        p1.exit()
+        p2.exit()
+        p3.exit()
