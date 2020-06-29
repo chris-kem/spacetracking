40d3dda9bd14c085f0d31a2cea02a92cf10ebb80
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-06-29 11:24:32 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,17 @@
 import subprocess
 import keyboard  # using module keyboard
 
-p1 = subprocess.run('python sensor/seriell-mqtt.py')
-p2 = subprocess.run('python', 'mqtt-linear.py')
-p3 = subprocess.run('python mqtt-binary.py')
+p1 = subprocess.Popen('python sensor/seriell-mqtt.py')
+p2 = subprocess.Popen('python', 'mqtt-linear.py')
+p3 = subprocess.Popen('python mqtt-binary.py')
 
 
 while True:  # making a loop
     try:  # used try so that if user pressed other than the given key error will not be shown
         if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-            p1.exit()
-            p2.exit()
-            p3.exit()
+            p1.Popen.kill()
+            p2.Popen.kill()
+            p3.Popen.kill()
             break  # finishing the loop
         else:
             pass
