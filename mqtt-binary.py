6fa07f4341b2d3640726f24bfd3e1157783d9fc2
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-03 10:14:01 +0200

    Update mqtt-binary.py

--- mqtt-binary.py
+++ mqtt-binary.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import cgi
 import json
 import time
 
@@ -15,6 +16,10 @@ value_laser1 = 0
 value_laser0 = 0
 value_laser2 = 0
 value_laser3 = 0
+
+form = cgi.FieldStorage()
+schwellenwert1 = form.getvalue("groesse1")
+print(schwellenwert1)
 
 def on_message(client, userdata, msg):
         global count_laser0, count_laser1, value_laser0, value_laser1, count_laser2, count_laser3, value_laser2, value_laser3
