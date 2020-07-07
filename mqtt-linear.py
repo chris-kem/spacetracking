5f6c38bc2679df43a4a81c3f0bebe6b8e1d11120
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-07 16:22:15 +0200

    Update mqtt-linear.py

--- mqtt-linear.py
+++ mqtt-linear.py
@@ -32,19 +32,19 @@ def on_message(client, userdata, msg):
 
     if sensorName.startswith("Sensor0", 3, 10):
         value = parsedMsg["Sensor0"] / 1020
-        mqttClient.publish("lasers/linear/laser0", round(float(value, 2)))
+        mqttClient.publish("lasers/linear/laser0", value)
 
     elif sensorName.startswith("Sensor1", 3, 10):
         value = parsedMsg["Sensor1"] / 1020
-        mqttClient.publish("lasers/linear/laser1", round(float(value, 2)))
+        mqttClient.publish("lasers/linear/laser1", value)
 
     elif sensorName.startswith("Sensor2", 3, 10):
         value = parsedMsg["Sensor2"] / 1020
-        mqttClient.publish("lasers/linear/laser2", round(float(value, 2)))
+        mqttClient.publish("lasers/linear/laser2", value)
 
     elif sensorName.startswith("Sensor3", 3, 10):
         value = parsedMsg["Sensor3"] / 1020
-        mqttClient.publish("lasers/linear/laser3", round(float(value, 2)))
+        mqttClient.publish("lasers/linear/laser3", value)
 
     else:
         mqttClient.publish("errors", value)
