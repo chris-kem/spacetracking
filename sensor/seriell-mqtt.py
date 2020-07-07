9164432e3112f02558700cc86817ddb2ab2b481b
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-07 12:40:08 +0200

    Update seriell-mqtt.py

--- sensor/seriell-mqtt.py
+++ sensor/seriell-mqtt.py
@@ -3,8 +3,10 @@ import serial
 import time
 import json
 
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+
 
 ser = serial.Serial("/dev/ttyACM0", 9600)
 time.sleep(5)
@@ -22,23 +24,23 @@ try:
         i = 0
         for word in response.split():
             zahl = float(word)
-            #print(zahl)
-            message_out = {"Sensor%d" % (i) : zahl}
+            print(zahl)
+            message_out = {"Sensor%d" % (i): zahl}
             parsedMsg = json.dumps(message_out)
             print(parsedMsg)
-	    if i == 0:
-	    	mqttClient.publish("lasers/raw/laser0", parsedMsg)
-	    elif i == 1:
-	    	mqttClient.publish("lasers/raw/laser1", parsedMsg)
-	    elif i == 2:
-	    	mqttClient.publish("lasers/raw/laser2", parsedMsg)
-	    elif i == 3:
-	    	mqttClient.publish("lasers/raw/laser3", parsedMsg)
-	    else:
-		mqttClient.publish("errors", "Fehler bei seriell zu mqtt")
+            if i == 0:
+                mqttClient.publish("lasers/raw/laser0", parsedMsg)
+            elif i == 1:
+                mqttClient.publish("lasers/raw/laser1", parsedMsg)
+            elif i == 2:
+                mqttClient.publish("lasers/raw/laser2", parsedMsg)
+            elif i == 3:
+                mqttClient.publish("lasers/raw/laser3", parsedMsg)
+            else:
+                mqttClient.publish("errors", "Fehler bei seriell zu mqtt")
 
 #	    mqttClient.loop_forever()
-	    i += 1
-     
+            i += 1
+
 except KeyboardInterrupt:
     ser.close()
