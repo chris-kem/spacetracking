0308ddad2c69b10ebb6d22e2fd427fa5c37f689c
chris-kem <chris.kem43@gmail.com>
2020-07-20 10:02:16 +0000

    something changed

---
+++ mqtt-lighthouse.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as mqtt
+import cgi
+import json
+import time
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+	mqttClient.subscribe("examples")
+
+def on_message(client, userdata, msg):
+	print(msg)
+
+	for word in msg.split(";", 3):
+		zahl = float(word)
+		print(zahl)
+        	#else:
+	    	#	mqttClient.publish("errors", value)
+	    	#	print("error %d" + value)
+
+       # print(value)
+
+mqttClient = mqtt.Client()
+mqttClient.on_connect = on_connect
+mqttClient.on_message = on_message
+mqttClient.connect("localhost", 1883, 60)
+mqttClient.loop_forever()
