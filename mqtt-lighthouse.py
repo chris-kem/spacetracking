c7e3fb5ffb581480ff84d70845653af2a0ac642b
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-21 09:20:38 +0200

    Update mqtt-lighthouse.py

--- mqtt-lighthouse.py
+++ mqtt-lighthouse.py
@@ -3,28 +3,31 @@ import cgi
 import json
 import time
 
+
 def on_connect(client, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
-	mqttClient.subscribe("examples")
+    print("Connected with result code "+str(rc))
+    mqttClient.subscribe("examples")
+
 
 def on_message(client, userdata, msg):
-#	print(str(msg.payload))
-#	print(msg.payload.decode("utf-8","ignore"))
-	parsedMsg = str(msg.payload.decode("utf-8","ignore"))
+    #	print(str(msg.payload))
+    #	print(msg.payload.decode("utf-8","ignore"))
+    parsedMsg = str(msg.payload.decode("utf-8", "ignore"))
 #	print(parsedMsg)
-	zahl = ["0","0","0"]
-	i = 0
-	for word in parsedMsg.split(";", 2):
-		zahl[i] = word
-		i += 1
-	
-	print(zahl[0] + " "+zahl[1] + " "+zahl[2] + " ")
-#		print(zahl)
-        	#else:
-	    	#	mqttClient.publish("errors", value)
-	    	#	print("error %d" + value)
+    zahl = ["0", "0", "0"]
+    i = 0
+    for word in parsedMsg.split(";", 3):
+        zahl[i] = word
+        i += 1
 
-       # print(value)
+    print(zahl[0] + " "+zahl[1] + " "+zahl[2] + " ")
+#		print(zahl)
+    # else:
+    #	mqttClient.publish("errors", value)
+    #	print("error %d" + value)
+
+   # print(value)
+
 
 mqttClient = mqtt.Client()
 mqttClient.on_connect = on_connect
