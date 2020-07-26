45aeb38401f34586388fffe2577139293572cf6c
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-26 12:15:04 +0200

    Update upload.php

--- html/upload.php
+++ html/upload.php
@@ -22,7 +22,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "svg" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "Sorry, SVG files are allowed.";
   $uploadOk = 0;
 }
 
