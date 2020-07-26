8227e6f0022beaad68d56d9d3d0d0749c6b06b31
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-26 16:57:05 +0200

    Update upload.php

--- html/upload.php
+++ html/upload.php
@@ -4,7 +4,8 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
+print_r($_FILES["fileToUpload"]["name"]);
+print_r($_FILES);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
