1ef56cd4d26dcebabc03a85400d3d2fe2fe09ba2
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-26 15:43:30 +0200

    Update download.php

--- html/download.php
+++ html/download.php
@@ -3,7 +3,6 @@ print_r("test");
 
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-print_r($target_file);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
