5967056d2cd06eb2aeef3f8504e52fb4cea55ee4
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-28 14:10:11 +0200

    harfenfunktion

--- html/upload.php
+++ html/upload.php
@@ -3,7 +3,7 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 //print_r($_FILES["fileToUpload"]["name"]);
 //print_r($_FILES);
 
@@ -22,7 +22,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 
 // Allow certain file formats
-if($imageFileType != "svg" ) {
+if ($imageFileType != "svg") {
   echo "Sorry, only SVG files are allowed.";
   $uploadOk = 0;
 }
@@ -30,10 +30,10 @@ if($imageFileType != "svg" ) {
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+  // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
     //header("Location: http://localhost/spacetracking");
   } else {
     echo "Sorry, there was an error uploading your file.";
