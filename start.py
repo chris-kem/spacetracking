9b6db6e3718a1ae75d169cb01f53ece27312d44b
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-07-30 09:14:28 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,11 @@
 import subprocess
 import time
 
-p1 = subprocess.Popen(['python', 'sensor/seriell-mqtt.py'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+p1 = subprocess.Popen(['python', 'seriell-mqtt.py'],
+                      stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 time.sleep(1)
-p2 = subprocess.Popen(['python', 'mqtt-linear.py'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['python', 'mqtt-linear.py'],
+                      stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 time.sleep(1)
 
 try:
