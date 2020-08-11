be0a69e3ff7d0eb69e5af7aeb0cfcc9b95e0c838
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-08-11 09:28:35 +0200

    changeSVG fixed and updated start.py

--- start.py
+++ start.py
@@ -1,17 +1,29 @@
 import subprocess
 import time
 
-p1 = subprocess.Popen(['python', 'seriell-mqtt.py'],
-                      stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+p1 = subprocess.Popen(
+    ["python", "seriell-mqtt.py"], stderr=subprocess.PIPE, stdout=subprocess.PIPE
+)
 time.sleep(1)
-p2 = subprocess.Popen(['python', 'mqtt-linear.py'],
-                      stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+p2 = subprocess.Popen(
+    ["python", "mqtt-linear.py"], stderr=subprocess.PIPE, stdout=subprocess.PIPE
+)
+time.sleep(1)
+p3 = subprocess.Popen(
+    ["python", "mqtt-OSC.py"], stderr=subprocess.PIPE, stdout=subprocess.PIPE
+)
+time.sleep(1)
+p4 = subprocess.Popen(
+    ["python", "track-OSC.py"], stderr=subprocess.PIPE, stdout=subprocess.PIPE
+)
 time.sleep(1)
 
 try:
     while True:
-        print('running')
+        print("running")
         time.sleep(10)
 except KeyboardInterrupt:
     p1.terminate()
     p2.terminate()
+    p3.terminate()
+    p4.terminate()
