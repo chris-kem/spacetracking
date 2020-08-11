0f940caad2b764d81c373b7ac6625120af1544d0
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-08-11 09:36:20 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,22 +1,5 @@
 import subprocess
 import time
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--localhost", help="write the hostname of the used computer")
-parser.add_argument("-p", "--port", type=int, help="which port to send OSC Data")
-args = parser.parse_args()
-
-if args.localhost:
-    hostname = args.localhost + ".local"
-    osc_client.connect((args.localhost, 9999))
-elif args.port:
-    osc_client.connect(("192.168.2.3", args.port))
-elif args.port and args.localhost:
-    hostname = args.localhost + ".local"
-    osc_client.connect((args.localhost, args.port))
-else:
-    osc_client.connect(("192.168.178.21", 9999))
 
 p1 = subprocess.Popen(
     ["python", "seriell-mqtt.py"], stderr=subprocess.PIPE, stdout=subprocess.PIPE
