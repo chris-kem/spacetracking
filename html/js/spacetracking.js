7741db1015327a6aa3f0861d13ee0551e0b44d36
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-08-11 08:53:55 +0200

    Update spacetracking.js

--- html/js/spacetracking.js
+++ html/js/spacetracking.js
@@ -11,9 +11,9 @@
 //
 $(document).ready(function () {
 
-  while (document.getElementById("dataSVG").value == "undefined" || "null") {
-    //setTimeout(console.log("kein SVG zum tracken verfügbar"), 1000);
-  }
+  //while (document.getElementById("dataSVG").value == "undefined" || "null") {
+  //setTimeout(console.log("kein SVG zum tracken verfügbar"), 1000);
+  //}
   var root = document.getElementById('dataSVG');
   //if (typeof root !== 'undefined' && root !== null) {
   var doc = root.contentDocument;
