8679c8f2a449aa18a19bd6fea03afd8d3d3a0afa
chris-kem <chris.kem43@gmail.com>
2020-08-14 16:11:22 +0000

    something changed

---
+++ messwerte/deca_messen.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import os
+
+#if os.path.exists("trackingdaten.txt"):
+#  os.remove("trackingdaten.txt")
+#else:
+#  print("The file does not exist") 
+
+
+datei = open('decadaten.txt','w')
+#datei.write("start")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+#    mqttClient.subscribe("examples")
+    mqttClient.subscribe("dwm/node/+/uplink/location")
+
+
+
+def on_message(client, userdata, msg):
+    parsedMsg = str(msg.payload.decode("utf-8", "ignore"))
+    #print(parsedMsg)
+    datei.write("\n" + parsedMsg)
+
+
+mqttClient = mqtt.Client()
+mqttClient.on_connect = on_connect
+mqttClient.on_message = on_message
+mqttClient.connect("localhost", 1883, 60)
+mqttClient.loop_forever()
