9ea41bd61bdeccb9ad44716be2c63a56c75ecfb2
chris-kem <chris.kem43@gmail.com>
2020-08-15 10:46:34 +0000

    something changed

--- messwerte/deca_messen.py
+++ messwerte/deca_messen.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import os
+import json
 
 #if os.path.exists("trackingdaten.txt"):
 #  os.remove("trackingdaten.txt")
@@ -13,15 +14,16 @@ datei = open('decadaten.txt','w')
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 #    mqttClient.subscribe("examples")
-    mqttClient.subscribe("dwm/node/+/uplink/location")
+    mqttClient.subscribe("dwm/node/db8e/uplink/location")
 
 
 
 def on_message(client, userdata, msg):
-    parsedMsg = str(msg.payload.decode("utf-8", "ignore"))
+    parsedMsg = json.loads(str(msg.payload.decode("utf-8", "ignore")))
     #print(parsedMsg)
-    datei.write("\n" + parsedMsg)
-
+    #datei.write("\n" + parsedMsg)
+    sensorName = str(parsedMsg["position"].keys())
+    print(sensorName)
 
 mqttClient = mqtt.Client()
 mqttClient.on_connect = on_connect
