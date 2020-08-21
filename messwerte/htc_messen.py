1a89a50715306b463cd343895e15b7ca19298298
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-08-21 17:00:39 +0200

    Update htc_messen.py

--- messwerte/htc_messen.py
+++ messwerte/htc_messen.py
@@ -2,33 +2,36 @@ import paho.mqtt.client as mqtt
 import os
 import time
 
-#if os.path.exists("trackingdaten.txt"):
+# if os.path.exists("trackingdaten.txt"):
 #  os.remove("trackingdaten.txt")
-#else:
-#  print("The file does not exist") 
+# else:
+#  print("The file does not exist")
 
 
-datei = open('trackingdaten.txt','w')
-#datei.write("start")
+datei = open('trackingdaten.txt', 'w')
+# datei.write("start")
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     mqttClient.subscribe("examples")
+
+
 #    mqttClient.subscribe("dwm/node/ViveTracker/uplink/location")
 start = time.time()
 while true:
-   try:
-	def on_message(client, userdata, msg):
-    		parsedMsg = str(msg.payload.decode("utf-8", "ignore"))
-    		print(parsedMsg)
-    		datei.write(parsedMsg)
+    try:
+        def on_message(client, userdata, msg):
+            parsedMsg = str(msg.payload.decode("utf-8", "ignore"))
+            print(parsedMsg)
+            datei.write(parsedMsg)
 
-	mqttClient = mqtt.Client()
-	mqttClient.on_connect = on_connect
-	mqttClient.on_message = on_message
-	mqttClient.connect("localhost", 1883, 60)
-	mqttClient.loop_forever()
+        mqttClient = mqtt.Client()
+        mqttClient.on_connect = on_connect
+        mqttClient.on_message = on_message
+        mqttClient.connect("localhost", 1883, 60)
+        mqttClient.loop_forever()
 
 except KeyboardInterrupt:
-        end = time.time()
-        print(end - start)
+    end = time.time()
+    print(end - start)
