0658b078ee5a464ce1e8713a9165a45119184bc0
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-08-31 09:39:25 +0200

    Update spacetracking.js

--- html/js/spacetracking.js
+++ html/js/spacetracking.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
     // for debugging and interaction
     //
     point.x = pos.x; point.y = pos.y;
-    point = point.matrixTransform(svg.getTransformToElement(marcer));
+    point = point.matrixTransform((marcer));
     if (marcer) {
       marcer.setAttribute('cx', point.x);
       marcer.setAttribute('cy', point.y);
