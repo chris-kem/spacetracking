eecb8d03b69df7a0cfcc9ee076fc73b82e60ad92
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-09-01 09:00:30 +0200

    Create keyboard.py

---
+++ html/keyboard.py
@@ -0,0 +1,31 @@
+from pynput.keyboard import Key, Controller
+import paho.mqtt.client as mqtt
+import json
+
+keyboard = Controller()
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("track")
+
+
+def on_message(client, userdata, msg):
+    parsedMsg = json.loads(msg.payload)
+    if parsedMsg["value"] == 0.1:
+        keyboard.press(Key.f1)
+        keyboard.release(Key.f1)
+    elif parsedMsg["value"] == 0.2:
+        keyboard.press(Key.f2)
+        keyboard.release(Key.f2)
+    elif parsedMsg["value"] == 0.3:
+        keyboard.press(Key.f3)
+        keyboard.release(Key.f3)
+    elif parsedMsg["value"] == 0.4:
+        keyboard.press(Key.f4)
+        keyboard.release(Key.f4)
+    elif parsedMsg["value"] == 0.5:
+        keyboard.press(Key.f5)
+        keyboard.release(Key.f5)
+    else:
+        print("kein feld")
