888f735825baa38ade6679322cb96a59f3a49bcc
chris-kem <56539007+chris-kem@users.noreply.github.com>
2020-09-01 09:00:32 +0200

    Update track-OSC.py

--- track-OSC.py
+++ track-OSC.py
@@ -1,44 +1,49 @@
 import paho.mqtt.client as mqtt
 import json
-import time
 import OSC
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-l","--localhost", help="write the hostname of the used computer")
-parser.add_argument("-p","--port", type=int, help="which port to send OSC Data")
-parser.add_argument("-s","--sensor", type=int, help="which sensor to send only")
+parser.add_argument("-l", "--localhost",
+                    help="write the hostname of the used computer")
+parser.add_argument("-p", "--port", type=int,
+                    help="which port to send OSC Data")
+parser.add_argument("-s", "--sensor", type=int,
+                    help="which sensor to send only")
 args = parser.parse_args()
 
 osc_client = OSC.OSCClient()
 
 if args.localhost:
-        hostname = args.localhost + ".local"
-        osc_client.connect((args.localhost, 9999))
+    hostname = args.localhost + ".local"
+    osc_client.connect((args.localhost, 9999))
 elif args.port:
-        osc_client.connect(("192.168.2.3", args.port))
+    osc_client.connect(("192.168.2.3", args.port))
 elif args.port and args.localhost:
-        hostname = args.localhost + ".local"
-        osc_client.connect((args.localhost, args.port))
+    hostname = args.localhost + ".local"
+    osc_client.connect((args.localhost, args.port))
 else:
-        osc_client.connect(("192.168.178.21", 9999))
+    osc_client.connect(("192.168.178.21", 9999))
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("track")
 
+
 def on_message(client, userdata, msg):
-	oscmsg = OSC.OSCMessage()
+    oscmsg = OSC.OSCMessage()
 #        print(type(msg))
-        parsedMsg = json.loads(msg.payload)
+    parsedMsg = json.loads(msg.payload)
 #        print(parsedMsg)
 #        print(parsedMsg["value"])
 #        print(parsedMsg.keys())
-	#if parsedMsg["elemid"] == "track1":
-	oscmsg.setAddress('{"/%s"}' % parsedMsg["elemid"])
-        oscmsg.append(parsedMsg["value"])
-	print(oscmsg)
-	osc_client.send(oscmsg)
+    # if parsedMsg["elemid"] == "track1":
+    oscmsg.setAddress('{"/%s"}' % parsedMsg["elemid"])
+    oscmsg.append(parsedMsg["value"])
+    print(oscmsg)
+    osc_client.send(oscmsg)
+
 
 mqttClient = mqtt.Client()
 mqttClient.on_connect = on_connect
